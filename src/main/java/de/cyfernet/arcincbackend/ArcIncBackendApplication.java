8dc006fd837fde659e38ad2bbaf5af2e74f9d796
Benjamin Wieczorek <benjamin.wieczorek@vanderlande.com>
2018-08-22 09:36:07 +0200

    Initial checkin

---
+++ src/main/java/de/cyfernet/arcincbackend/ArcIncBackendApplication.java
@@ -0,0 +1,12 @@
+package de.cyfernet.arcincbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcIncBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcIncBackendApplication.class, args);
+	}
+}
